8b5f2d6a1cfd06b11b97fc9c2cc4a346577fc9a1
Anneka Goss <goss@work.co>
2020-05-26 20:08:42 -0400

    load state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,70 +7,129 @@ import DepthPage from '../pages/DepthPage';
 import InteractionPage from '../pages/InteractionPage';
 import TransitionPage from '../pages/TransitionPage';
 import DOMRasterizationPage from '../pages/DOMRasterizationPage';
+import Loader from './Loader/Loader';
+
 import styles from './app.module.scss';
 
 const App = () => {
 	const [activePageIndex, setActivePageIndex] = React.useState<number>(0);
-	if (!glSupported()) return <div>'WebGL is not supported on this device.'</div>;
+	const [isLoaded, setIsLoaded] = React.useState<boolean>(false);
+	if (!glSupported())
+		return <div>'WebGL is not supported on this device.'</div>;
+
+	React.useEffect(() => {
+		setTimeout(() => {
+			setIsLoaded(true);
+		}, 0);
+	}, []);
 
 	return (
-		<div className={styles.app}>
-			<nav className={styles.navigation}>
-				<ul>
-					<div
-						className={cx(styles.navItem, activePageIndex === 0 && styles.active)}
-						onClick={() => {
-							setActivePageIndex(0);
-						}}>
-						0. Form
-					</div>
-					<div
-						className={cx(styles.navItem, activePageIndex === 1 && styles.active)}
-						onClick={() => {
-							setActivePageIndex(1);
-						}}>
-						1. Motion
-					</div>
-					<div
-						className={cx(styles.navItem, activePageIndex === 2 && styles.active)}
-						onClick={() => {
-							setActivePageIndex(2);
-						}}>
-						2. Depth
-					</div>
-					<div
-						className={cx(styles.navItem, activePageIndex === 3 && styles.active)}
-						onClick={() => {
-							setActivePageIndex(3);
-						}}>
-						3. Interaction
-					</div>
-					<div
-						className={cx(styles.navItem, activePageIndex === 4 && styles.active)}
-						onClick={() => {
-							setActivePageIndex(4);
-						}}>
-						4. Transition
-					</div>
-					<div
-						className={cx(styles.navItem, activePageIndex === 5 && styles.active)}
-						onClick={() => {
-							setActivePageIndex(5);
-						}}>
-						5. DOM Rasterization
-					</div>
-				</ul>
-			</nav>
-			<div className={styles.PagesContainer}>
-				<FormPage isActive={activePageIndex === 0} />
-				<MotionPage isActive={activePageIndex === 1} />
-				<DepthPage isActive={activePageIndex === 2} />
-				<InteractionPage isActive={activePageIndex === 3} />
-				<TransitionPage isActive={activePageIndex === 4} />
-				<DOMRasterizationPage isActive={activePageIndex === 5} />
+		<>
+			<div className={styles.app}>
+				{isLoaded && (
+					<Nav
+						activePageIndex={activePageIndex}
+						setActivePageIndex={setActivePageIndex}
+					/>
+				)}
+				{!isLoaded && <Loader />}
+				{isLoaded && <Pages activePageIndex={activePageIndex} />}
 			</div>
-		</div>
+		</>
 	);
 };
+
+interface NavProps {
+	activePageIndex: number;
+	setActivePageIndex: (index: number) => void;
+}
+
+const Nav = ({ activePageIndex, setActivePageIndex }: NavProps) => (
+	<nav className={styles.navigation}>
+		<ul>
+			<div
+				className={cx(
+					styles.navItem,
+					activePageIndex === 0 && styles.active
+				)}
+				onClick={() => {
+					setActivePageIndex(0);
+				}}
+			>
+				0. Form
+			</div>
+			<div
+				className={cx(
+					styles.navItem,
+					activePageIndex === 1 && styles.active
+				)}
+				onClick={() => {
+					setActivePageIndex(1);
+				}}
+			>
+				1. Motion
+			</div>
+			<div
+				className={cx(
+					styles.navItem,
+					activePageIndex === 2 && styles.active
+				)}
+				onClick={() => {
+					setActivePageIndex(2);
+				}}
+			>
+				2. Depth
+			</div>
+			<div
+				className={cx(
+					styles.navItem,
+					activePageIndex === 3 && styles.active
+				)}
+				onClick={() => {
+					setActivePageIndex(3);
+				}}
+			>
+				3. Interaction
+			</div>
+			<div
+				className={cx(
+					styles.navItem,
+					activePageIndex === 4 && styles.active
+				)}
+				onClick={() => {
+					setActivePageIndex(4);
+				}}
+			>
+				4. Transition
+			</div>
+			<div
+				className={cx(
+					styles.navItem,
+					activePageIndex === 5 && styles.active
+				)}
+				onClick={() => {
+					setActivePageIndex(5);
+				}}
+			>
+				5. DOM Rasterization
+			</div>
+		</ul>
+	</nav>
+);
+
+interface PagesProps {
+	activePageIndex: number;
+}
+
+const Pages = ({ activePageIndex }: PagesProps) => (
+	<div className={styles.PagesContainer}>
+		<FormPage isActive={activePageIndex === 0} />
+		<MotionPage isActive={activePageIndex === 1} />
+		<DepthPage isActive={activePageIndex === 2} />
+		<InteractionPage isActive={activePageIndex === 3} />
+		<TransitionPage isActive={activePageIndex === 4} />
+		<DOMRasterizationPage isActive={activePageIndex === 5} />
+	</div>
+);
 
 export default App;
--- src/components/BaseCanvas.tsx
+++ src/components/BaseCanvas.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
-import { UniformSettings, Vector2, MESH_TYPE, LoadedShaders } from '../../types';
-import { assignUniforms, loadShader, mapUniformSettingsToLocations } from '../../lib/gl/initialize';
+import {
+	UniformSettings,
+	Vector2,
+	MESH_TYPE,
+	LoadedShaders,
+} from '../../types';
+import {
+	assignUniforms,
+	loadShader,
+	mapUniformSettingsToLocations,
+} from '../../lib/gl/initialize';
 import { BASE_TRIANGLE_MESH } from '../../lib/gl/settings';
 import { useInitializeGL } from '../hooks/gl';
 import { useAnimationFrame } from '../hooks/animation';
@@ -27,27 +36,50 @@ interface RenderProps {
 	texture?: HTMLImageElement;
 }
 
-const render = ({ gl, uniformLocations, uniforms, time, mousePos }: RenderProps) => {
+const render = ({
+	gl,
+	uniformLocations,
+	uniforms,
+	time,
+	mousePos,
+}: RenderProps) => {
 	if (!gl) return;
 	assignUniforms(uniforms, uniformLocations, gl, time, mousePos);
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 };
 
-const BaseCanvas = ({ fragmentShader, vertexShader, uniforms, setAttributes, setFragmentError, setVertexError }: Props) => {
-	const canvasRef: React.RefObject<HTMLCanvasElement> = React.useRef<HTMLCanvasElement>();
+const BaseCanvas = ({
+	fragmentShader,
+	vertexShader,
+	uniforms,
+	setAttributes,
+	setFragmentError,
+	setVertexError,
+}: Props) => {
+	const canvasRef: React.RefObject<HTMLCanvasElement> = React.useRef<
+		HTMLCanvasElement
+	>();
 	const size: React.MutableRefObject<Vector2> = React.useRef<Vector2>({
 		x: uniforms.current.uResolution.value.x,
 		y: uniforms.current.uResolution.value.y,
 	});
-	const initialMousePosition = uniforms.current.uMouse ? uniforms.current.uMouse.defaultValue : { x: 0.5, y: 0.5 };
+	const initialMousePosition = uniforms.current.uMouse
+		? uniforms.current.uMouse.defaultValue
+		: { x: 0.5, y: 0.5 };
 	const mousePosRef: React.MutableRefObject<Vector2> = React.useRef<Vector2>({
 		x: size.current.x * initialMousePosition.x,
 		y: size.current.y * -initialMousePosition.y,
 	});
 	const gl = React.useRef<WebGLRenderingContext>();
-	const programRef: React.MutableRefObject<WebGLProgram> = React.useRef<WebGLProgram>();
-	const loadedShadersRef: React.MutableRefObject<LoadedShaders> = React.useRef<LoadedShaders>({ fragmentShader: null, vertexShader: null });
-	const uniformLocations = React.useRef<Record<string, WebGLUniformLocation>>();
+	const programRef: React.MutableRefObject<WebGLProgram> = React.useRef<
+		WebGLProgram
+	>();
+	const loadedShadersRef: React.MutableRefObject<LoadedShaders> = React.useRef<
+		LoadedShaders
+	>({ fragmentShader: null, vertexShader: null });
+	const uniformLocations = React.useRef<
+		Record<string, WebGLUniformLocation>
+	>();
 
 	useInitializeGL({
 		gl,
@@ -63,10 +95,22 @@ const BaseCanvas = ({ fragmentShader, vertexShader, uniforms, setAttributes, set
 	});
 
 	React.useEffect(() => {
-		setAttributes([{ name: 'aVertexPosition', value: BASE_TRIANGLE_MESH.join(', ') }]);
+		setAttributes([
+			{ name: 'aVertexPosition', value: BASE_TRIANGLE_MESH.join(', ') },
+		]);
 	}, []);
 
-	useUpdateShaders({ gl, programRef, loadedShadersRef, uniformLocations, uniforms, fragmentShader, vertexShader, setFragmentError, setVertexError });
+	useUpdateShaders({
+		gl,
+		programRef,
+		loadedShadersRef,
+		uniformLocations,
+		uniforms,
+		fragmentShader,
+		vertexShader,
+		setFragmentError,
+		setVertexError,
+	});
 
 	useWindowSize(canvasRef, gl, uniforms.current, size);
 	useMouse(mousePosRef, canvasRef);
@@ -81,7 +125,14 @@ const BaseCanvas = ({ fragmentShader, vertexShader, uniforms, setAttributes, set
 		});
 	});
 
-	return <canvas ref={canvasRef} width={size.current.x} height={size.current.y} role='img' />;
+	return (
+		<canvas
+			ref={canvasRef}
+			width={size.current.x}
+			height={size.current.y}
+			role='img'
+		/>
+	);
 };
 
 export default BaseCanvas;
