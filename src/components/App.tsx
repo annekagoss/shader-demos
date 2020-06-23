359098603399c97c94a6a46febec773917fe7328
Anneka Goss <annekagoss@gmail.com>
2020-06-23 10:20:27 -0400

    default page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -13,7 +13,7 @@ import Loader from './Loader/Loader';
 import styles from './app.module.scss';
 
 const App = () => {
-	const [activePageIndex, setActivePageIndex] = React.useState<number>(2);
+	const [activePageIndex, setActivePageIndex] = React.useState<number>(0);
 	const [isLoaded, setIsLoaded] = React.useState<boolean>(false);
 	if (!glSupported()) return <div>'WebGL is not supported on this device.'</div>;
 
